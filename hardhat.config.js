f9d610d40684dd85a56571ea8433d7a24fe03b8d
tomonakar <tomohisa.nak@gmail.com>
2021-11-11 17:20:48 +0900

    chore: pkeyをhardhat.configに追加

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,10 @@
 require("@nomiclabs/hardhat-waffle")
 const projectId = "95fdbb6b356846af99ce14ffde9a6a14"
+const fs = require("fs")
+const keyData = fs.readFileSync("./p-key.txt", {
+  encoding: "utf8",
+  flag: "r",
+})
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -9,11 +14,11 @@ module.exports = {
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: [],
+      accounts: [keyData],
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${projectId}`,
-      accounts: [],
+      accounts: [keyData],
     },
   },
   solidity: {
