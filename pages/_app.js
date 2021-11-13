b4819610c396654f1b95d8331ef44b80bc0a4065
tomonakar <tomohisa.nak@gmail.com>
2021-11-13 22:22:46 +0900

    feat: _appを更新

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import "./app.css"
+import Link from "next/link"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function KryptoBirdMarketplace({ Component, pageProps }) {
+  return (
+    <div>
+      <nav className="border-b p-6" style={{ backgroundColor: "purple" }}>
+        <p cpassName="text-4x1 font-bold text-white">NFT マーケットプレイス</p>
+        <div className="flex mt-4 justify-center"></div>
+        <Link href="/">
+          <a className="mr-4">Mainマーケットプレイス</a>
+        </Link>
+      </nav>
+    </div>
+  )
 }
 
-export default MyApp
+export default KryptoBirdMarketplace
