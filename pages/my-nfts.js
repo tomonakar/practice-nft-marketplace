811febda03049e9f82801746f307e4802245d2a2
tomonakar <tomohisa.nak@gmail.com>
2021-11-15 17:17:34 +0900

    feat: NFTマーケット機能

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -1,3 +1,5 @@
+// we want to load the users nfts and display
+
 import { ethers } from "ethers"
 import { useEffect, useState } from "react"
 import axios from "axios"
@@ -8,7 +10,8 @@ import { nftaddress, nftmarketaddress } from "../config"
 import NFT from "../artifacts/contracts/NFT.sol/NFT.json"
 import KBMarket from "../artifacts/contracts/KBMarket.sol/KBMarket.json"
 
-export default function Home() {
+export default function MyAssets() {
+  // array of nfts
   const [nfts, setNFts] = useState([])
   const [loadingState, setLoadingState] = useState("not-loaded")
 
@@ -18,16 +21,20 @@ export default function Home() {
 
   async function loadNFTs() {
     // what we want to load:
-    // ***provider, tokenContract, marketContract, data for our marketItems***
+    // we want to get the msg.sender hook up to the signer to display the owner nfts
 
-    const provider = new ethers.providers.JsonRpcProvider()
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(
       nftmarketaddress,
       KBMarket.abi,
-      provider,
+      signer,
     )
-    const data = await marketContract.fetchMarketTokens()
+    const data = await marketContract.fetchMyNFTs()
 
     const items = await Promise.all(
       data.map(async (i) => {
@@ -52,29 +59,12 @@ export default function Home() {
     setLoadingState("loaded")
   }
 
-  // function to buy nfts for market
-
-  async function buyNFT(nft) {
-    const web3Modal = new Web3Modal()
-    const connection = await web3Modal.connect()
-    const provider = new ethers.providers.Web3Provider(connection)
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(nftmarketaddress, KBMarket.abi, signer)
-
-    const price = ethers.utils.parseUnits(nft.price.toString(), "ether")
-    const transaction = await contract.createMarketSale(
-      nftaddress,
-      nft.tokenId,
-      {
-        value: price,
-      },
-    )
-
-    await transaction.wait()
-    loadNFTs()
-  }
   if (loadingState === "loaded" && !nfts.length)
-    return <h1 className="px-20 py-7 text-4x1">No NFts in marketplace</h1>
+    return (
+      <h1 className="px-20 py-7 text-4x1">
+        You do not own any NFTs currently :(
+      </h1>
+    )
 
   return (
     <div className="flex justify-center">
@@ -98,12 +88,6 @@ export default function Home() {
                 <p className="text-3x-1 mb-4 font-bold text-white">
                   {nft.price} ETH
                 </p>
-                <button
-                  className="w-full bg-purple-500 text-white font-bold py-3 px-12 rounded"
-                  onClick={() => buyNFT(nft)}
-                >
-                  Buy
-                </button>
               </div>
             </div>
           ))}
